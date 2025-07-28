6c1db86d7d88450783283a1cb3baef7bdf4cf42e
Dhaarun <149036248+95003@users.noreply.github.com>
2025-07-28 11:51:10 +0530

    Delete auto_pro directory

--- auto_pro/install_splunk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget -O splunk.tgz https://download.splunk.com/products/splunk/releases/9.1.2/linux/splunk-9.1.2-a7f645ddaf91-Linux-x86_64.tgz
-tar -xvzf splunk.tgz -C /opt
-useradd splunk
-chown -R splunk:splunk /opt/splunk
-/opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt
-/opt/splunk/bin/splunk enable boot-start
--- auto_pro/variable.tf
+++
@@ -1,4 +0,0 @@
-variable "region" {}
-variable "key_name" {}
-variable "instance_count" {}
-variable "install_splunk" {}

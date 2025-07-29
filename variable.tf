3cd6f31f02b9849e3c9375f5a45da2e6ff86bd5b
Dhaarun <149036248+95003@users.noreply.github.com>
2025-07-29 16:24:51 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,26 +1,26 @@
- variable "region" {
+variable "region" {
   description = "AWS region"
   type        = string
 }
 
 variable "key_name" {
-  description = "Name of the existing EC2 key pair"
+  description = "EC2 key pair name"
   type        = string
 }
 
 variable "instance_count" {
-  description = "Number of EC2 instances"
+  description = "Number of instances"
   type        = number
   default     = 1
 }
 
 variable "install_splunk" {
-  description = "Install Splunk (yes or no)"
-  type        = string
-  default     = "no"
+  description = "Whether to install Splunk"
+  type        = bool
+  default     = false
 }
 
 variable "s3_bucket_name" {
-  description = "Name of S3 bucket to store keys"
+  description = "S3 bucket name to store keys"
   type        = string
-} 
+}
